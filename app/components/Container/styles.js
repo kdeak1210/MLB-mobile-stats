fbb042414869cdf074f8d8302a1d191448ea6a9c
Kyle Deak <kdeak1210@gmail.com>
2018-04-07 19:32:23 -0400

    Setup initial app structure
    React Native app code will be in the app/ directory, project root at app/index.js. Inside the app directory is screens/ which will hold the different screens a user can navigate to. There is also a components/ directory, which will hold React Native components in their own folders grouped by feature

---
+++ app/components/Container/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+/** Take up the whole screen (flex: 1)
+ *  Center align vertical and horizontally
+ */
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
---
+++ app/screens/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { Container } from '../components/Container';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class Home extends React.Component {
+  render() {
+    return (
+      <Container>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </Container>
+    );
+  }
+}
+
+export default Home;
