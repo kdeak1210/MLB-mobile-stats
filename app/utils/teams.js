ff9bb5b64b09b5b1e66cdf44c32015dcc99f5c05
Kyle Deak <kdeak1210@gmail.com>
2018-04-19 19:20:40 -0400

    Fixed the sorting utility function
    Amended the compareTeams comparison function to return 1, -1, or 0 instead of a boolean. Now the teams array is sorted properly, and in descending order in terms of a team's win percentage from that season

--- app/utils/teams.js
+++ app/utils/teams.js
@@ -1,7 +1,15 @@
-// compare two teams for sorting on Win % ('win_p' from SportsRadar API)
-export const compareTeams = (team1, team2) => team1.win_p > team2.win_p;
+// Sort the teams DESC on win_p property (win percentage from SportRadar API)
+export const compareTeams = (team1, team2) => {
+  if (team1.win_p < team2.win_p) {
+    return 1;
+  }
+  if (team1.win_p > team2.win_p) {
+    return -1;
+  }
+  return 0;
+};
 
-// Flatten a standings response from SportsRadar into an array of teams
+// Flatten a standings response from SportsRadar into an array of teams, then sort on win_p
 export const arrayFromStandings = standings => {
   const teamsArr = [];
   standings.league.season.leagues.forEach(league => {
@@ -9,6 +17,8 @@ export const arrayFromStandings = standings => {
       teamsArr.push(...division.teams);
     });
   });
+
+  teamsArr.sort(compareTeams);
 
   return teamsArr;
 };
